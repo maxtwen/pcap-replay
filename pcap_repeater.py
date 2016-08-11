b0e1ff80916b8e53cf3be3a3ea665a7ac12a4b93
maxim <maxtwen@yandex.ru>
2016-08-12 01:02:15 +0300

    add: context managers, exceptions handling

--- pcap_repeater.py
+++ pcap_repeater.py
@@ -1,26 +1,47 @@
+from contextlib import contextmanager, closing
+
 import dpkt
 import sys
 import socket
 
 
-if __name__ == '__main__':
-    path = sys.argv
+@contextmanager
+def ignored(excpts):
+    try:
+        yield
+    except excpts:
+        pass
+
+
+def pcap_handler(path, timeout=1):
     for ts, pkt in dpkt.pcap.Reader(open(path, 'r')):
         eth = dpkt.ethernet.Ethernet(pkt)
-        dst_ip = socket.inet_ntoa(eth.ip.dst)
-        src_ip = socket.inet_ntoa(eth.ip.src)
-        dst_port = eth.ip.data.dport
-        src_port = eth.ip.data.sport
-        data = eth.ip.data.data
+        if eth.type == dpkt.ethernet.ETH_TYPE_IP:
+            dst_ip = socket.inet_ntoa(eth.ip.dst)
+            # src_ip = socket.inet_ntoa(eth.ip.src)
+            dst_port = eth.ip.data.dport
+            # src_port = eth.ip.data.sport
+            data = eth.ip.data.data
 
-        if type(eth.ip.data) == dpkt.udp.UDP:
-            protocol = socket.SOCK_DGRAM
-        elif type(eth.ip.data) == dpkt.tcp.TCP:
-            protocol = socket.SOCK_STREAM
-        else:
-            raise TypeError
+            if data is '':  # python caches and reuses short strings
+                continue
 
-        sock_client = socket.socket(socket.AF_INET, protocol)
-        sock_client.connect((dst_ip, dst_port))
-        sock_client.send(data)
-        sock_client.recv(0)
+            if type(eth.ip.data) == dpkt.udp.UDP:
+                protocol = socket.SOCK_DGRAM
+            elif type(eth.ip.data) == dpkt.tcp.TCP:
+                protocol = socket.SOCK_STREAM
+            else:
+                raise TypeError
+
+            with ignored(socket.timeout), closing(socket.socket(socket.AF_INET, protocol)) as sock_client:
+                sock_client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                sock_client.settimeout(timeout)
+                sock_client.connect((dst_ip, dst_port))
+                sock_client.send(data)
+                sock_client.shutdown(socket.SHUT_WR)
+                sock_client.recv(0)
+
+if __name__ == '__main__':
+    args = sys.argv
+    pcap_handler(args[1], args[2])
+
